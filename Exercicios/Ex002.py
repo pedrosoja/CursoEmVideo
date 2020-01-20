8ff1a9e1264335baf959b2dc593758c0b0bfcddb
pedrosoja <pedrosoja@hotmail.com>
2020-01-20 14:11:53 -0300

    Initial commit

---
+++ Exercicios/Ex002.py
@@ -0,0 +1,8 @@
+#####################################################
+##Curso em Vídeo
+##Aula 002
+##https://youtu.be/FNqdV5Zb_5Q
+#####################################################
+name = input("Digite seu nome: ")
+print("Ola, {}".format(name))
+print("Seja bem vindo")
