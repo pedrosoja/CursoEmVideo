1e308bc2dfda56983ae91c02bf3f5f8ee4970e83
pedrosoja <pedrosoja@hotmail.com>
2020-01-21 11:25:52 -0300

    Atualizando o Ex010 para inserir a cotação do dia anterior

--- Ex010.py
+++ Ex010.py
@@ -4,8 +4,10 @@
 ##https://youtu.be/xM4AX3Lp2mo
 #####################################################
 import requests
+from datetime import datetime, timedelta
 real = float(input('Quanto dinheiro você tem na carteira? : R$ '))
-req = requests.get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='USD'&@dataCotacao='01-20-2020'&$top=100&$filter=tipoBoletim%20eq%20'Fechamento%20PTAX'&$format=json&$select=cotacaoVenda,dataHoraCotacao,tipoBoletim")
+yesterday = datetime.strftime(datetime.now() - timedelta(1), '%m-%d-%Y')
+req = requests.get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='USD'&@dataCotacao='" + yesterday + "'&$top=100&$filter=tipoBoletim%20eq%20'Fechamento%20PTAX'&$format=json&$select=cotacaoVenda,dataHoraCotacao,tipoBoletim")
 cotação = req.json()
 cotação_valor = cotação['value']
 tx_conversão = float(cotação_valor[0]['cotacaoVenda'])
